45ade91bb29c56ceb72d51873d57004934a5c5f0
MassimoAzzini <massimo.azzini.web@gmail.com>
2024-03-27 16:52:07 +0100

    [add] ferragnez party

---
+++ src/org/learning/ferragnez/FerragnezParty.java
@@ -0,0 +1,39 @@
+package org.learning.ferragnez;
+
+import java.util.Scanner;
+
+public class FerragnezParty {
+    public static void main(String[] args) {
+        String[] guests = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+        Scanner keyboardReader = new Scanner(System.in);
+        System.out.print("Come ti chiami?");
+        String guestName = keyboardReader.nextLine();
+
+        System.out.println(guestName);
+
+        boolean inList = false;
+
+        for (int i = 0; i < guests.length; i++) {
+            if (guestName.equalsIgnoreCase(guests[i])){
+                inList = true;
+            }
+        }
+
+        if (inList){
+            System.out.println("Benvenuti alla festa!!");
+        } else {
+            System.out.println("Mi dispiace ma non sei in lista.");
+        }
+
+
+
+
+
+
+
+
+
+        keyboardReader.close();
+    }
+}
